ded824e3a64163664de04aeb3386b9ed1bff4ee3
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2022-01-24 21:26:54 +0200

    looks good

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import UserFormUsingState from "./components/Users/UserForm";
+// import UserFormUsingState from "./components/Users/UserForm";
 import { useState } from "react";
 import UsersList from "./components/Users/UsersList";
 import UserFormUsingRef from './components/Users/UserFormUsingRef';
@@ -9,11 +9,20 @@ function App() {
   
   return (
     <div className="text-center">
-      {/* <UserFormUsingState setUsers={setUsers} />  */} {/*using State when you need to read and Write the input data and wanna change it (more code) using onchangr and value attributes*/}
-      <UserFormUsingRef setUsers={setUsers} /> {/*using Ref when you just read the input data and do not want to change it (less code)*/}
+      {/* <UserFormUsingState setUsers={setUsers} />  */} 
+      {/*using State when you need to read and Write the input data and wanna change it (more code) using onchangr and value attributes*/}
+      <UserFormUsingRef setUsers={setUsers} /> 
+      {/*using Ref when you just read the input data and do not want to change it (less code)*/}
       <UsersList users={users} setUsers={setUsers} />
     </div>
   );
 }
+// NOTES: 
+// useRef do not re-render the component but State do.
+// useRef most often used in counter and reference elements (input)
+// useRef store the previous value of the state
+// using Ref make the component Uncontrolled component but state make it controlled one
+// controlled component => Internal State is Controlled By React
+// Uncontrolled component => controlled by DOM
 
 export default App;
